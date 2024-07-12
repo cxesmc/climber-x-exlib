2f25dce0a2dffcc1ddfb5b23ad844e01d69dabd0
Alexander Robinson <alexander.robinson@awi.de>
2024-07-12 17:29:31 +0200

    Added pik install script.

---
+++ install_pik.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Make sure we stop on errors
+set -e 
+
+COMPILER_OPTS_DEFAULT=""
+COMPILER_OPTS_IFX="CC=icx F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+
+COMPILER_OPTS=$COMPILER_OPTS_DEFAULT
+
+if [[ $1 = "ifx" ]]; then
+    COMPILER_OPTS=$COMPILER_OPTS_IFX
+fi
+
+echo "COMPILER_OPTS = $COMPILER_OPTS"
+echo ""
+
+### FFTW ###
+
+SRCDIR=$PWD
+FFTWSRC=fftw-3.3.10
+
+# with omp enabled
+cd $FFTWSRC
+eval "./configure --disable-doc --prefix=$SRCDIR/exlib/fftw-omp --enable-openmp $COMPILER_OPTS"
+make clean
+make
+make install
+cd $SRCDIR
+
+# serial (without omp) 
+cd $FFTWSRC
+eval "./configure --disable-doc --prefix=$SRCDIR/exlib/fftw-serial $COMPILER_OPTS"
+make clean
+make
+make install
+cd $SRCDIR
+
+### LIS ###
+
+module load intel/oneAPI/2023.2.0 #(error when compiling with most recent intel OneAPI 2024.0)
+
+SRCDIR=$PWD
+LISSRC=lis-2.1.6
+
+# with omp enabled
+cd $LISSRC
+eval "./configure --prefix=$SRCDIR/exlib/lis-omp --enable-f90 --enable-omp $COMPILER_OPTS"
+make clean
+make
+make install
+cd $SRCDIR
+
+# serial (without omp) 
+cd $LISSRC
+eval "./configure --prefix=$SRCDIR/exlib/lis-serial --enable-f90 $COMPILER_OPTS" 
+make clean
+make
+make install
+cd $SRCDIR
+
+module load intel/oneAPI/2024.0.0 #(to revert previous change)
+
+echo ""
+echo "" 
+./check.sh
+echo ""
