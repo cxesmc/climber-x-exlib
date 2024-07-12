7747a67adc43c5190b61c87d651988e6999cba9f
Alexander Robinson <alexander.robinson@awi.de>
2024-07-12 17:36:05 +0200

    Cleaned install scripts.

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Make sure we stop on errors
 set -e 
 
-COMPILER_OPTS_DEFAULT=""
-COMPILER_OPTS_IFX="CC=icx F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
-
-COMPILER_OPTS=$COMPILER_OPTS_DEFAULT
-
+# Set compiler options
 if [[ $1 = "ifx" ]]; then
-    COMPILER_OPTS=$COMPILER_OPTS_IFX
+    COMPILER_OPTS="CC=icx F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+elif [[ $1 = "ifort" ]]; then
+    COMPILER_OPTS="CC=icc F77=ifort 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+else
+    COMPILER_OPTS=""
 fi
 
 echo "COMPILER_OPTS = $COMPILER_OPTS"
@@ -38,10 +38,6 @@ cd $SRCDIR
 
 ### LIS ###
 
-if [[ $2 = "pik" ]]; then
-    module load intel/oneAPI/2023.2.0 #(error when compiling with most recent intel OneAPI 2024.0)
-fi
-
 SRCDIR=$PWD
 LISSRC=lis-2.1.6
 
@@ -60,11 +56,6 @@ make clean
 make
 make install
 cd $SRCDIR
-
-
-if [[ $2 = "pik" ]]; then
-    module load intel/oneAPI/2024.0.0 #(to revert previous change)
-fi
 
 echo ""
 echo "" 
--- install_pik.sh
+++ install_pik.sh
@@ -3,13 +3,13 @@
 # Make sure we stop on errors
 set -e 
 
-COMPILER_OPTS_DEFAULT=""
-COMPILER_OPTS_IFX="CC=icx F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
-
-COMPILER_OPTS=$COMPILER_OPTS_DEFAULT
-
+# Set compiler options
 if [[ $1 = "ifx" ]]; then
-    COMPILER_OPTS=$COMPILER_OPTS_IFX
+    COMPILER_OPTS="CC=icx F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+elif [[ $1 = "ifort" ]]; then
+    COMPILER_OPTS="CC=icc F77=ifort 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+else
+    COMPILER_OPTS=""
 fi
 
 echo "COMPILER_OPTS = $COMPILER_OPTS"
