d68f32ba032a7cfb8a08d8015535aeb2869b34a4
Alexander Robinson <alexander.robinson@awi.de>
2024-07-16 11:26:07 +0200

    Updating dkrz install script following IT-help advice.

--- install_dkrz.sh
+++ install_dkrz.sh
@@ -8,7 +8,8 @@ set -e
 
 # Set compiler options
 if [[ $1 = "ifx" ]]; then
-    COMPILER_OPTS="F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+    #COMPILER_OPTS="F77=ifx 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
+    COMPILER_OPTS="CC=icx F77=ifx ac_cv_c_libs='-L/sw/spack-levante/intel-oneapi-compilers-2023.2.1-kfv7xx/compiler/2023.2.1/linux/compiler/lib/intel64_lin -L/sw/spack-levante/intel-oneapi-compilers-2023.2.1-kfv7xx/compiler/2023.2.1/linux/bin-llvm/../lib -L/sw/spack-levante/gcc-11.2.0-bcn7mb/lib/gcc/x86_64-pc-linux-gnu/11.2.0 -L/sw/spack-levante/gcc-11.2.0-bcn7mb/lib/gcc/x86_64-pc-linux-gnu/11.2.0/../../../../lib64 -L/lib/../lib64 -L/usr/lib/../lib64 -L/sw/spack-levante/gcc-11.2.0-bcn7mb/lib/gcc/x86_64-pc-linux-gnu/11.2.0/../../.. -L/lib -L/usr/lib -L/sw/spack-levante/intel-oneapi-compilers-2023.2.1-kfv7xx/compiler/2023.2.1/linux/lib -lsvml -lirng -limf -lm -lgcc_s -lirc -ldl -lirc_s'"
 elif [[ $1 = "ifort" ]]; then
     COMPILER_OPTS="CC=icc F77=ifort 'FFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback' 'CFLAGS=-Ofast -march=core-avx2 -mtune=core-avx2 -traceback'"
 else
